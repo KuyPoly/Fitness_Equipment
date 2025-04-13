89b33670092ee937d10fcaaaa2c843210f4f96e2
KuyPoly <kuypoly297@gmail.com>
2025-04-13 20:24:53 +0700

    change responsive

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import './home.css';
 import Landpage from '../../component/landpage/landpage';
 import PremiumCard from '../../component/premuimCard/premuimCard';
 import Wear from '../../component/wear/wear';
+import { Link } from 'react-router-dom';
 import ProCard from '../../component/pro_card/pro_card';
 import Footer from '../../component/footer/footer';
 
@@ -13,27 +14,52 @@ import pre3 from '../../assets/pre3.png';
 import pre4 from '../../assets/pre4.png';
 
 export default function Home() {
-    return (
-        <div className="home">
-            <div className="home__container">
-                <Landpage />
-                <div className="home__premium-section">
-                    <h3>Premium Equipment</h3>
-                    <div className="home__premium-cards">
-                        <PremiumCard name="Gym Racks" link="/rackpage" pic={pre1} />
-                        <PremiumCard name="Weight Bench" link="/" pic={pre2} />
-                        <PremiumCard name="Bar&Weight" link="/" pic={pre3} />
-                        <PremiumCard name="Specialty Machines" link="/" pic={pre4} />
-                    </div>
-                </div>
-                <Wear />
-                <div className='home__bestseller__container'>
-                    <ProCard />
-                </div>
-                <div className="home__footer">
-                    <Footer />
-                </div>
+  const carouselRef = useRef();
+
+  const scroll = (direction) => {
+    if (carouselRef.current) {
+      const scrollAmount = direction === 'left' ? -300 : 300;
+      carouselRef.current.scrollBy({ left: scrollAmount, behavior: 'smooth' });
+    }
+  };
+
+  return (
+    <div className="home">
+      <div className="home__container">
+        <Landpage />
+        <div className="home__premium-section">
+          <h3>Premium Equipment</h3>
+          <div className="carousel-container">
+            <button
+              className="carousel-arrow prev"
+              onClick={() => scroll('left')}
+              aria-label="Scroll left"
+            >
+              &lt;
+            </button>
+            <div className="home__premium-cards" ref={carouselRef}>
+              <PremiumCard name="Gym Racks" link="/" pic={pre1} />
+              <PremiumCard name="Weight Bench" link="/" pic={pre2} />
+              <PremiumCard name="Bar&Weight" link="/" pic={pre3} />
+              <PremiumCard name="Specialty Machines" link="/" pic={pre4} />
             </div>
+            <button
+              className="carousel-arrow next"
+              onClick={() => scroll('right')}
+              aria-label="Scroll right"
+            >
+              &gt;
+            </button>
+          </div>
         </div>
-    );
+        <Wear />
+        <div>
+            <ProCard /> 
+        </div>
+        <div>
+            <Footer/>
+        </div>
+      </div>
+    </div>
+  );
 }
